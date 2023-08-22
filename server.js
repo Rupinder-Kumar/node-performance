486daaa67b81b0d80f93cecced408ac4ebde0559
Rupinder <rupinder3972@gmail.com>
2023-08-22 09:13:52 +0530

    implemented pm2 for auto managing workers

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
 const app = express();
 
 function delay(duration) {
     const starTime = Date.now();
-    while(Date.now() - starTime > duration) {
+    while (Date.now() - starTime > duration) {
         // event loop is blocked...
     }
- }
+}
 
 app.get('/', (req, res) => {
     res.send(`Performance Example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
-    delay(9000);
+    delay(4000);
     res.send(`Ding ding ding: ${process.pid}`)
 });
 
 console.log("Running Server.js")
-if(cluster.isMaster) {
-    console.log("Master has been started");
-    const NUM_WORKERS = os.cpus().length;
-    console.log(NUM_WORKERS);
-    for(i = 0;i < NUM_WORKERS;i++){
-        cluster.fork();
-    }
-} else {
-    console.log("Worker has been started")
-    app.listen(3000);
-}
+console.log("Worker has been started")
+app.listen(3000);
